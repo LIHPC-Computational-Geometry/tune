0fe937533ac7ce007e5ab85922de16bccb55ceba
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2024-03-28 15:35:00 +0100

    Issue #4: Json format reading

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from view.window import Game
+
+import sys
+import json
 import model.Linear2CMap
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    nodes = [(0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0)]
-    faces = [(0, 1, 2), (0, 2, 3)]
+    if len(sys.argv) != 2:
+        print("Usage: main.py <mesh_file.json>")
+    else:
+        f = open(sys.argv[1])
+        json_mesh = json.load(f)
+        nodes = json_mesh['nodes']
+        faces = json_mesh['faces']
+        print("Nodes: " + str(nodes))
+        print("Faces: " + str(faces))
 
-    cmap = model.Linear2CMap.Mesh(nodes, faces)
+        cmap = model.Linear2CMap.Mesh(nodes, faces)
 
-#TODO Remove the mesh from the main.
-# The game is built from the cmap directly
-    g = Game(cmap)
-    g.run()
+        #TODO Remove the mesh from the main.
+        # The game is built from the cmap directly
+        g = Game(cmap)
+        g.run()
