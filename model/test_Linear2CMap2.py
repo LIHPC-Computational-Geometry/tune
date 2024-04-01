7b6e9b91656818d36eb319625096924718fd4158
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2024-04-01 09:05:01 +0200

    Simple unit test to compare nodes and dart_info arrays

---
+++ model/test_Linear2CMap2.py
@@ -0,0 +1,25 @@
+import unittest
+import Linear2CMap
+
+class TestLinear2CMap(unittest.TestCase):
+
+    def test_triangle_json(self):
+        # mesh with one triangle in json
+        json_mesh = {
+            "nodes": [[0.0, 1.0], [1.0, 1.0], [1.0, 0.0]],
+            "faces": [[2, 0, 1]]
+        }
+        json_cmap = Linear2CMap.Mesh(json_mesh['nodes'], json_mesh['faces'])
+
+        # same mesh programmatically
+        cmap = Linear2CMap.Mesh()
+        n0 = cmap.add_node(0, 1)
+        n1 = cmap.add_node(1, 1)
+        n2 = cmap.add_node(1, 0)
+        cmap.add_triangle(n2, n0, n1)
+
+        self.assertEquals(json_cmap.nodes, cmap.nodes)
+        self.assertEquals(json_cmap.dart_info, cmap.dart_info)
+
+if __name__ == '__main__':
+    unittest.main()
