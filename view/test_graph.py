37371d1e967a97129571549e6482c578dd15a46d
franck.ledoux <franck.ledoux@live.fr>
2024-04-09 06:49:29 +0200

    Line reformatting to fulfill coadcy code style advises

--- view/test_graph.py
+++ view/test_graph.py
@@ -2,6 +2,7 @@ import unittest
 from pygame import math
 from graph import Edge
 
+
 class TestGraph(unittest.TestCase):
 
     def test_Edge_collide_point(self):
@@ -10,6 +11,7 @@ class TestGraph(unittest.TestCase):
         self.assertTrue(Edge.is_pt_on_segment(0.0, 0.0, 1.0, 0.0, p1, tolerance))
         p2 = math.Vector2(1.5, 0.0)
         self.assertFalse(Edge.is_pt_on_segment(0.0, 0.0, 1.0, 0.0, p2, tolerance))
+
 
 if __name__ == '__main__':
     unittest.main()
