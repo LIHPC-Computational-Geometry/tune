219b42146a6f607b1e114ff49366a1ef4a114302
ropercha <arzhela.rp@gmail.com>
2025-01-09 10:42:31 +0100

    Fix test issues

--- test_modules/test_mesh_analysis.py
+++ test_modules/test_mesh_analysis.py
@@ -12,7 +12,7 @@ class TestMeshAnalysis(unittest.TestCase):
         nodes = [[0.0, 0.0], [1.0, 0.0], [0.5, 1], [-0.5, 1.0], [0.0, 2.0], [-1.0,0.0],[-0.5,-1.0],[0.0,-2.0], [0.5,-1.0]]
         faces = [[0, 1, 2], [0, 2, 3], [3, 2, 4], [3, 5, 0], [0, 5, 6], [0, 6, 8], [6, 7, 8], [0, 8, 1]]
         cmap = Mesh(nodes,faces)
-        nodes_score, mesh_score, mesh_ideal_score = Mesh_analysis.global_score(cmap)
+        nodes_score, mesh_score, mesh_ideal_score, adjacency = Mesh_analysis.global_score(cmap)
         self.assertEqual((0,0), (mesh_score,mesh_ideal_score) )  # add assertion here
 
     def test_mesh_with_irregularities(self):
@@ -23,14 +23,14 @@ class TestMeshAnalysis(unittest.TestCase):
                  [3, 4, 5], [3, 5, 6], [3, 6, 7], [7, 6, 8], [7, 8, 9], [7, 9, 10], [10, 9, 11], [10, 11, 12],
                  [14, 12, 13], [14, 13, 15], [1, 14, 15], [1, 15, 16], [1, 16, 17], [1, 17, 2], [2, 17, 18], [2, 18, 4]]
         cmap = Mesh(nodes, faces)
-        nodes_score, mesh_score, mesh_ideal_score = Mesh_analysis.global_score(cmap)
+        nodes_score, mesh_score, mesh_ideal_score, adjacency = Mesh_analysis.global_score(cmap)
         self.assertEqual((6, -2), (mesh_score,mesh_ideal_score) )  # add assertion here
 
     def test_mesh_bad_score(self):
         nodes = [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [2.0, 0.0]]
         faces = [[0, 1, 2], [0, 2, 3], [1, 4, 2]]
         cmap = Mesh(nodes, faces)
-        nodes_score, mesh_score, mesh_ideal_score = Mesh_analysis.global_score(cmap)
+        nodes_score, mesh_score, mesh_ideal_score, adjacency = Mesh_analysis.global_score(cmap)
         self.assertEqual((3, 1), (mesh_score, mesh_ideal_score))
 
     def test_split_score(self):
@@ -39,8 +39,8 @@ class TestMeshAnalysis(unittest.TestCase):
         cmap = Mesh(nodes, faces)
         split_edge_ids(cmap, 0, 2)
         split_edge_ids(cmap, 1, 2) # split impossible
-        nodes_score, mesh_score, mesh_ideal_score = Mesh_analysis.global_score(cmap)
-        self.assertEqual((3, 1), (mesh_score, mesh_ideal_score))
+        nodes_score, mesh_score, mesh_ideal_score, adjacency = Mesh_analysis.global_score(cmap)
+        self.assertEqual((5, 1), (mesh_score, mesh_ideal_score))
 
     def test_find_template_opposite_node_not_found(self):
         nodes = [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [2.0, 0.0]]
@@ -57,16 +57,16 @@ class TestMeshAnalysis(unittest.TestCase):
         nodes = [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [2.0, 0.0]]
         faces = [[0, 1, 2], [0, 2, 3], [1, 4, 2]]
         cmap = Mesh(nodes, faces)
-        self.assertEqual(Mesh_analysis.isValidAction(cmap, 0, 3), False)
-        self.assertEqual(Mesh_analysis.isValidAction(cmap, 2, 0), True)
-        self.assertEqual(Mesh_analysis.isValidAction(cmap, 2, 3), False)
+        self.assertEqual(Mesh_analysis.isValidAction(cmap, 0, 3), (False, True))
+        self.assertEqual(Mesh_analysis.isValidAction(cmap, 2, 0), (True, True))
+        self.assertEqual(Mesh_analysis.isValidAction(cmap, 2, 3), (False, False))
 
     def test_isFlipOk(self):
         nodes = [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [2.0, 0.0]]
         faces = [[0, 1, 2], [0, 2, 3], [1, 4, 2]]
         cmap = Mesh(nodes, faces)
         dart_to_test = Dart(cmap, 0)
-        self.assertFalse(Mesh_analysis.isFlipOk(dart_to_test))
+        self.assertFalse(Mesh_analysis.isFlipOk(dart_to_test)[0])
         dart_to_test = Dart(cmap, 2)
         self.assertTrue(Mesh_analysis.isFlipOk(dart_to_test))
 
@@ -76,7 +76,7 @@ class TestMeshAnalysis(unittest.TestCase):
         cmap = Mesh(nodes, faces)
         plot_mesh(cmap)
         dart_to_test = Dart(cmap, 0)
-        self.assertFalse(Mesh_analysis.isSplitOk(dart_to_test))
+        self.assertFalse(Mesh_analysis.isSplitOk(dart_to_test)[0])
         dart_to_test = Dart(cmap, 2)
         self.assertTrue(Mesh_analysis.isSplitOk(dart_to_test))
 
@@ -86,9 +86,9 @@ class TestMeshAnalysis(unittest.TestCase):
         cmap = Mesh(nodes, faces)
         plot_mesh(cmap)
         dart_to_test = Dart(cmap, 0)
-        self.assertFalse(Mesh_analysis.isCollapseOk(dart_to_test))
+        self.assertFalse(Mesh_analysis.isCollapseOk(dart_to_test)[0])
         dart_to_test = Dart(cmap, 2)
-        self.assertFalse(Mesh_analysis.isCollapseOk(dart_to_test))
+        self.assertFalse(Mesh_analysis.isCollapseOk(dart_to_test)[0])
 
 
 if __name__ == '__main__':
